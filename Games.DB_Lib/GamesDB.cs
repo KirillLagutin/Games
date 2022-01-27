f8f4c74ac0a26fcb4f5b980b272fbfa2494c77d1
Kirill Lagutin <lagutin.k@mail.ru>
2022-01-27 23:28:47 +0300

    Update

---
+++ Games.DB_Lib/GamesDB.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Games.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Games.DB_Lib
+{
+    public class GamesDB : DbContext
+    {
+        public DbSet<Game> TableGames { get; set; }
+
+        public GamesDB()
+        {
+            Database.EnsureCreated();
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(GetConnectionString("connection_to_db.txt"));
+        }
+
+        private static string GetConnectionString(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
