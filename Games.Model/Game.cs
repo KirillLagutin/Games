33d9738997f24faeafd06ba2be9454b5eb3f4a20
Kirill Lagutin <lagutin.k@mail.ru>
2022-01-27 21:46:08 +0300

    Add Games.Model

---
+++ Games.Model/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Games.Model
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Style { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
