fea5d5c3e1e7f4892d25476ac0fda6ee5ac907c6
Kirill Lagutin <lagutin.k@mail.ru>
2022-01-27 23:27:57 +0300

    Update

--- Games.App/Program.cs
+++ Games.App/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using Games.DB_Lib;
+using Games.Model;
 
 namespace Games.App
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var db = new GamesDB();
+
+            foreach (var game in db.TableGames)
+            {
+                Console.WriteLine($"#{game.Id}: {game.Name} {game.Company} {game.Style} {game.Date}");
+            }
+
+            db.TableGames.Add(new Game
+            {
+                Name = "Stalker 2",
+                Company = "GSC",
+                Style = "First person shooter",
+                Date = "08/12/2022"
+            });
+            db.SaveChanges();
+            
+            foreach (var game in db.TableGames)
+            {
+                Console.WriteLine($"#{game.Id}: {game.Name} {game.Company} {game.Style} {game.Date}");
+            }
         }
     }
 }
--- Games.Model/Game.cs
+++ Games.Model/Game.cs
@@ -1,13 +1,28 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Games.Model
 {
+    [Table("tab_games")]
     public class Game
     {
+        [Column("id")]
         public int Id { get; set; }
+        
+        [Column("name")]
+        [Required]
         public string Name { get; set; }
+        
+        [Column("company")]
+        [Required]
         public string Company { get; set; }
+        
+        [Column("style")]
+        [Required]
         public string Style { get; set; }
-        public DateTime Date { get; set; }
+        
+        [Column("date")]
+        [Required]
+        public string Date { get; set; }
     }
 }
